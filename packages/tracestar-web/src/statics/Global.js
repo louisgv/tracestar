e10bcd65cf4ca014cbc4ee76b8b70deb4bfa5393
Joe Johans <lgvichy@gmail.com>
2018-04-24 11:41:10 -0400

    New URL for the backend

--- packages/tracestar-web/src/statics/Global.js
+++ packages/tracestar-web/src/statics/Global.js
@@ -16,7 +16,7 @@ app.Global = Object.freeze({
     TWO_PI: Math.PI * 2,
     API_URL: window.location.host === 'localhost:3000'
         ? 'http://localhost:8080'
-        : 'https://tracestar-server-tcfbssvdrd.now.sh',
+        : 'https://tracestar-server-stkycmxkds.now.sh',
     COLOR: {
         BLANK: "#EEEEEE",
         WALL: "#333333",
